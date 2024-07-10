49b6226433fe686f30fef62d65843beac964db87
gouthamtirumala <157142011+gouthamtirumala@users.noreply.github.com>
2024-07-10 16:12:20 +0530

    Add files via upload

---
+++ Email.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
+import numpy as np
+st.image(r"C:\Users\HP\OneDrive\Desktop\inno12.png")
+model = pickle.load(open(r"C:\Users\HP\AI ELITE 20\nb.pkl","rb"))
+with open(r"C:\Users\HP\AI ELITE 20\bow.pkl","rb") as f:
+      bow=pickle.load(f)
+st.title("MAIL SPAM-HAM CLASSIFIER")
+email = st.text_input("enter the Email:")
+if email:
+      Data = bow.transform([email]).toarray()
+      Result = model.predict(Data)[0]
+if st.button("predict"):
+      st.write("this email is a:",Result)
+      if Result =="spam":
+             st.image(r"C:\Users\HP\OneDrive\Desktop\spam2.jpg")
+      else:
+           st.image(r"C:\Users\HP\OneDrive\Desktop\ham.jpg") 
