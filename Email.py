60d94e23ee8662baa0f319cddaa2159ab4415440
gouthamtirumala <157142011+gouthamtirumala@users.noreply.github.com>
2024-07-10 16:33:51 +0530

    Update Email.py

--- Email.py
+++ Email.py
@@ -5,8 +5,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pickle
 import numpy as np
 st.image(r"C:\Users\HP\OneDrive\Desktop\inno12.png")
-model = pickle.load(open(r"C:\Users\HP\AI ELITE 20\nb.pkl","rb"))
-with open(r"C:\Users\HP\AI ELITE 20\bow.pkl","rb") as f:
+model = pickle.load(open("nb.pkl","rb"))
+with open("bow.pkl","rb") as f:
       bow=pickle.load(f)
 st.title("MAIL SPAM-HAM CLASSIFIER")
 email = st.text_input("enter the Email:")
